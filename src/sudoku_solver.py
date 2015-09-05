65925219da7d0d41cde5ecffe8a661ea898cc984
Konstantin Semenov <konstantin.semenov@vrmtechnology.co.uk>
2015-09-05 20:12:55 +0100

    Simple solutions

--- src/sudoku_solver.py
+++ src/sudoku_solver.py
@@ -3,15 +3,26 @@ import numpy as np
 
 
 class SudokuSolver(object):
-    def __init__(self, input):
-        self._input = np.matrix(input)
+    def __init__(self, input_):
+        self._input = np.matrix(input_)
 
     def solution(self):
         return self._input
 
     def solution_for_row(self, row_number):
         """finds solution for given row"""
-        pass
+        row = self._input.tolist()[row_number]
+        template = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+        diff = template.difference(set(row))
+        if len(diff) == 1:
+            row[row.index(0)] = diff.pop()
+        elif len(diff) > 1:
+            while row.count(0) >= 1:
+                zero_index = row.index(0)
+                column = self.solution_for_column(zero_index).tolist()
+                column_list = [sub_item for item in column for sub_item in item]
+                row[zero_index] = column_list[row_number]
+        return np.matrix(row)
 
     def is_row_filled_in(self, row_number):
         row = self._input[row_number]
@@ -22,20 +33,19 @@ class SudokuSolver(object):
         return not (column == np.matrix('0; 0; 0; 0; 0; 0; 0; 0; 0')).any()
 
     def is_square_filled_in(self, square_number):
-        return True
-
+        return not (self.fetch_square(square_number) == np.zeros((3, 3))).any()
 
     def fetch_square(self, square_number):
         square_coordinates = self.coordinates_for_square(square_number)
-        square = self._input[]
-        pass
+        return self._input[:, square_coordinates['col']][square_coordinates['row'], :]
 
-    def coordinates_for_square(self, square_number):
+    @staticmethod
+    def coordinates_for_square(square_number):
         if square_number in (0, 3, 6):
             col = (0, 1, 2)
         elif square_number in (1, 4, 7):
             col = (3, 4, 5)
-        elif square_number in (2, 5, 8):
+        else:
             col = (6, 7, 8)
         if square_number < 3:
             row = (0, 1, 2)
@@ -45,8 +55,27 @@ class SudokuSolver(object):
             row = (6, 7, 8)
         return {'row': row, 'col': col}
 
+    def solution_for_column(self, column_number):
+        row = self._input[:, column_number].tolist()
+        row = [sub_item for item in row for sub_item in item]
+        template = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+        diff = template.difference(set(row))
+        if len(diff):
+            row[row.index(0)] = diff.pop()
+        return np.matrix(';'.join([str(i) for i in row]))
 
-
-
-
-
+    def solution_for_square(self, square_number):
+        square = self.fetch_square(square_number).tolist()
+        flat_square = [sub_item for item in square for sub_item in item]
+        template = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+        diff = template.difference(set(flat_square))
+        if len(diff):
+            for index, row in enumerate(square):
+                try:
+                    zero = row.index(0)
+                    if zero >= 0:
+                        square[index][zero] = diff.pop()
+                except ValueError:
+                    pass
+                    # ignore
+        return np.matrix(square)
--- test/test_sudoku.py
+++ test/test_sudoku.py
@@ -3,13 +3,21 @@ import numpy
 
 from src.sudoku_solver import SudokuSolver
 
-INCOMPLETE_SOLUTION = '0 9 5 7 4 3 8 6 1; 4 3 1 8 6 5 9 2 7; 8 7 6 1 9 2 5 4 3; ' \
-    '3 8 7 4 5 9 2 1 6; 6 1 2 3 8 7 4 9 5; 5 4 9 2 1 6 7 3 8; ' \
-    '7 6 3 5 3 4 1 8 9; 9 2 8 6 7 1 3 5 4; 1 5 4 9 3 8 6 7 2 '
+SOLUTION_WITHOUT_ONE_VALUE = '0 9 5 7 4 3 8 6 1; 4 3 1 8 6 5 9 2 7; 8 7 6 1 9 2 5 4 3; ' \
+                             '3 8 7 4 5 9 2 1 6; 6 1 2 3 8 7 4 9 5; 5 4 9 2 1 6 7 3 8; ' \
+                             '7 6 3 5 3 4 1 8 9; 9 2 8 6 7 1 3 5 4; 1 5 4 9 3 8 6 7 2 '
+
+SOLUTION_WITHOUT_TWO_VALUES_IN_ROW = '0 9 5 7 4 3 0 6 1; 4 3 1 8 6 5 9 2 7; 8 7 6 1 9 2 5 4 3; ' \
+                                     '3 8 7 4 5 9 2 1 6; 6 1 2 3 8 7 4 9 5; 5 4 9 2 1 6 7 3 8; ' \
+                                     '7 6 3 5 3 4 1 8 9; 9 2 8 6 7 1 3 5 4; 1 5 4 9 3 8 6 7 2 '
+
+SOLUTION_WITHOUT_TWO_VALUES_IN_COLUMN = '0 9 5 7 4 3 8 6 1; 0 3 1 8 6 5 9 2 7; 8 7 6 1 9 2 5 4 3; ' \
+                                        '3 8 7 4 5 9 2 1 6; 6 1 2 3 8 7 4 9 5; 5 4 9 2 1 6 7 3 8; ' \
+                                        '7 6 3 5 3 4 1 8 9; 9 2 8 6 7 1 3 5 4; 1 5 4 9 3 8 6 7 2 '
 
 SOLUTION = '2 9 5 7 4 3 8 6 1; 4 3 1 8 6 5 9 2 7; 8 7 6 1 9 2 5 4 3; ' \
-    '3 8 7 4 5 9 2 1 6; 6 1 2 3 8 7 4 9 5; 5 4 9 2 1 6 7 3 8; ' \
-    '7 6 3 5 3 4 1 8 9; 9 2 8 6 7 1 3 5 4; 1 5 4 9 3 8 6 7 2 '
+           '3 8 7 4 5 9 2 1 6; 6 1 2 3 8 7 4 9 5; 5 4 9 2 1 6 7 3 8; ' \
+           '7 6 3 5 3 4 1 8 9; 9 2 8 6 7 1 3 5 4; 1 5 4 9 3 8 6 7 2 '
 
 SQUARE0 = '2 9 5; 4 3 1; 8 7 6'
 
@@ -17,7 +25,6 @@ SQUARE0 = '2 9 5; 4 3 1; 8 7 6'
 class CompleteSudokuTests(TestCase):
     def setUp(self):
         self.solver = SudokuSolver(SOLUTION)
-
 
     def test_solved_solution(self):
         self.assertTrue(numpy.array_equal(self.solver.solution(), numpy.matrix(SOLUTION)))
@@ -33,64 +40,82 @@ class CompleteSudokuTests(TestCase):
 
     def test_square_array_slice(self):
         """testing for slice of main sudoku square that gets returned. maps to the square"""
-        slice = self.solver.coordinates_for_square(0)
-        self.assertEqual(slice['row'], (0, 1, 2))
-        self.assertEqual(slice['col'], (0, 1, 2))
+        _slice = self.solver.coordinates_for_square(0)
+        self.assertEqual(_slice['row'], (0, 1, 2))
+        self.assertEqual(_slice['col'], (0, 1, 2))
 
-        slice = self.solver.coordinates_for_square(1)
-        self.assertEqual(slice['row'], (0, 1, 2))
-        self.assertEqual(slice['col'], (3, 4, 5))
+        _slice = self.solver.coordinates_for_square(1)
+        self.assertEqual(_slice['row'], (0, 1, 2))
+        self.assertEqual(_slice['col'], (3, 4, 5))
 
-        slice = self.solver.coordinates_for_square(2)
-        self.assertEqual(slice['row'], (0, 1, 2))
-        self.assertEqual(slice['col'], (6, 7, 8))
+        _slice = self.solver.coordinates_for_square(2)
+        self.assertEqual(_slice['row'], (0, 1, 2))
+        self.assertEqual(_slice['col'], (6, 7, 8))
 
-        slice = self.solver.coordinates_for_square(3)
-        self.assertEqual(slice['row'], (3, 4, 5))
-        self.assertEqual(slice['col'], (0, 1, 2))
+        _slice = self.solver.coordinates_for_square(3)
+        self.assertEqual(_slice['row'], (3, 4, 5))
+        self.assertEqual(_slice['col'], (0, 1, 2))
 
-        slice = self.solver.coordinates_for_square(4)
-        self.assertEqual(slice['row'], (3, 4, 5))
-        self.assertEqual(slice['col'], (3, 4, 5))
+        _slice = self.solver.coordinates_for_square(4)
+        self.assertEqual(_slice['row'], (3, 4, 5))
+        self.assertEqual(_slice['col'], (3, 4, 5))
 
-        slice = self.solver.coordinates_for_square(5)
-        self.assertEqual(slice['row'], (3, 4, 5))
-        self.assertEqual(slice['col'], (6, 7, 8))
+        _slice = self.solver.coordinates_for_square(5)
+        self.assertEqual(_slice['row'], (3, 4, 5))
+        self.assertEqual(_slice['col'], (6, 7, 8))
 
-        slice = self.solver.coordinates_for_square(6)
-        self.assertEqual(slice['row'], (6, 7, 8))
-        self.assertEqual(slice['col'], (0, 1, 2))
+        _slice = self.solver.coordinates_for_square(6)
+        self.assertEqual(_slice['row'], (6, 7, 8))
+        self.assertEqual(_slice['col'], (0, 1, 2))
 
-        slice = self.solver.coordinates_for_square(7)
-        self.assertEqual(slice['row'], (6, 7, 8))
-        self.assertEqual(slice['col'], (3, 4, 5))
+        _slice = self.solver.coordinates_for_square(7)
+        self.assertEqual(_slice['row'], (6, 7, 8))
+        self.assertEqual(_slice['col'], (3, 4, 5))
 
-        slice = self.solver.coordinates_for_square(8)
-        self.assertEqual(slice['row'], (6, 7, 8))
-        self.assertEqual(slice['col'], (6, 7, 8))
+        _slice = self.solver.coordinates_for_square(8)
+        self.assertEqual(_slice['row'], (6, 7, 8))
+        self.assertEqual(_slice['col'], (6, 7, 8))
 
     def test_square_complete(self):
         self.assertTrue(self.solver.is_square_filled_in(0))
 
+    def test_solution_for_row(self):
+        self.assertTrue((self.solver.solution_for_row(0) == numpy.matrix(SOLUTION[0:17])).all())
+
+    def test_solution_for_column(self):
+        solution = numpy.matrix(SOLUTION)
+        self.assertTrue((self.solver.solution_for_column(0) == solution[:, 0]).all())
+
+    def test_solution_for_square(self):
+        solution = numpy.matrix(SOLUTION)
+        self.assertTrue((self.solver.solution_for_square(0) == solution[(0, 1, 2), :][:, (0, 1, 2)]).all())
 
 
-class IncompleteSudokuTests(TestCase):
+class SingleMissingValueSudokuTests(TestCase):
     def setUp(self):
-        self.solver = SudokuSolver(INCOMPLETE_SOLUTION)
+        self.solver = SudokuSolver(SOLUTION_WITHOUT_ONE_VALUE)
 
     def test_row_incomplete(self):
         self.assertFalse(self.solver.is_row_filled_in(0))
 
-    @skip('need the row level solution')
+    def test_square_incomplete(self):
+        self.assertFalse(self.solver.is_square_filled_in(0))
+
     def test_solution_for_row(self):
-        self.assertEqual(self.solver.solution_for_row(0), SOLUTION[0:17])
+        self.assertTrue((self.solver.solution_for_row(0) == numpy.matrix(SOLUTION[0:17])).all())
+
+    def test_solution_for_column(self):
+        expected = numpy.matrix(SOLUTION)
+        self.assertTrue((self.solver.solution_for_column(0) == expected[:, 0]).all())
 
     def test_column_incomplete(self):
         self.assertFalse(self.solver.is_column_filled_in(0))
 
-    @skip('need to fetch a square by number')
-    def test_square_incomplete(self):
-        self.assertFalse(self.solver.is_square_filled_in(0))
+    def test_solution_for_square(self):
+        solution = numpy.matrix(SOLUTION)
+        self.assertTrue((self.solver.solution_for_square(0) == solution[(0, 1, 2), :][:, (0, 1, 2)]).all())
 
 
-
+class TwoMissingValuesInRowSudokuTests(SingleMissingValueSudokuTests):
+    def setUp(self):
+        self.solver = SudokuSolver(SOLUTION_WITHOUT_TWO_VALUES_IN_ROW)
